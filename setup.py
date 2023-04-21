57d7ce10b0b473fd967c1bd4f9d68ea705ccb804
Gabriel <80380712+gabecolli@users.noreply.github.com>
2023-04-21 14:35:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='testpackage',
-    version='1.0',
-    author='Gabriel',
-    author_email='gabriel@example.com',
-    description='A test package with no dependencies',
-    url='https://github.com/gabecolli/testpackage',
-    packages=find_packages(),
+import setuptools
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="test-package",
+    version="0.0.1",
+    author="Gabriel",
+    author_email="gabriel@example.com",
+    packages=["test_package"],
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabecolli/testpackage",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[]
 )
 
