598b585d3216ef449bedab25e856748ad0f8a5b2
Gabriel <80380712+gabecolli@users.noreply.github.com>
2023-04-21 12:06:12 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="testpackage",
-    version="1.0.0",
-    author="Gabriel Colli",
-    author_email="gabe@email.com",
-    description="this is a test package",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/gabecolli/testpackage",
+    name='testpackage',
+    version='1.0',
+    author='Gabriel',
+    author_email='gabriel@example.com',
+    description='A test package with no dependencies',
+    url='https://github.com/gabecolli/testpackage',
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        # list of required packages
-    ],
 )
+
